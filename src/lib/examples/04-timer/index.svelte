e0e83abe617bfb6d3775cac7414bdfd319285a64
indra saputra idrus <indrasaputraidrus@gmail.com>
2024-11-04 12:21:49 +0800

    06-circle-draw

--- src/lib/examples/04-timer/index.svelte
+++ src/lib/examples/04-timer/index.svelte
@@ -5,34 +5,43 @@
 
 	function start() {
 		interval = setInterval(() => {
-			elapsed += 0.1;
 			if (elapsed > duration) {
 				elapsed = duration;
 				clearInterval(interval);
 			}
+			elapsed = Math.min(elapsed + 0.1, duration);
 		}, 100);
 	}
 
 	function reset() {
+		clearInterval(interval);
 		elapsed = 0;
 		start();
 	}
 
 	$effect(() => {
+		clearInterval(interval);
+
+		// Directly adjust elapsed to fit within the new duration
+		if (elapsed > duration) {
+			elapsed = duration;
+		}
+
 		if (!duration) return;
 		start();
 	});
 
+	$inspect({ duration, elapsed });
 </script>
 
-<section class="flex items-center justify-center h-full">
+<section class="flex h-full items-center justify-center overflow-x-hidden">
 	<div class="space-y-6">
 		<h1 class="text-2xl font-bold">Timer</h1>
 
-		<div class="space-y-3 w-72">
+		<div class="w-72 space-y-3">
 			<div class="flex items-center gap-2">
 				<span>Elapsed Time:</span>
-				<div class="h-3 flex-1 border rounded">
+				<div class="h-3 flex-1 rounded border">
 					<div style="width: {(elapsed / duration) * 100}%;" class="h-full bg-teal-200"></div>
 				</div>
 			</div>
@@ -43,7 +52,7 @@
 				<input class="flex-1" type="range" bind:value={duration} min="1" max="10" />
 			</label>
 
-			<button onclick={reset} class="bg-teal-200 text-teal-700 w-full p-2 rounded">Reset</button>
+			<button onclick={reset} class="w-full rounded bg-teal-200 p-2 text-teal-700">Reset</button>
 		</div>
 	</div>
 </section>
--- src/lib/examples/06-circle-draw/index.svelte
+++ src/lib/examples/06-circle-draw/index.svelte
@@ -12,7 +12,16 @@
 	let circles = $state<Circle[]>([]);
 	let selected = $state<Circle>()!;
 
+	let snapshots: Circle[][] = [];
+	let history = $state(-1);
+
 	function drawCircle(e: MouseEvent) {
+		if (status === 'editing') {
+			snapshot();
+			status = 'drawing';
+			return;
+		}
+
 		const svgEl = e.target as SVGElement;
 		const { left, top } = svgEl.getBoundingClientRect();
 
@@ -25,17 +34,44 @@
 
 		circles.push(circle);
 		selected = circle;
+		snapshot();
+	}
+
+	function undo() {
+		circles = snapshots[--history];
+	}
+
+	function redo() {
+		circles = snapshots[++history];
+	}
+
+	function snapshot() {
+		history++;
+		snapshots.push($state.snapshot(circles));
 	}
 </script>
 
 <section class="flex h-full items-center justify-center">
 	<div>
 		<h1 class="mb-4 text-center text-2xl font-semibold">Cirle Draw</h1>
-		<div class="space-y-4">
+		<div class="relative space-y-4">
 			<div class="items-enter flex justify-center gap-2">
-				<button>Undo</button>
-				<button>Redo</button>
+				<button onclick={undo} disabled={history === -1}>Undo</button>
+				<button onclick={redo} disabled={history === snapshots.length - 1}>Redo</button>
 			</div>
+
+			{#if status === 'editing'}
+				<div
+					class="absolute bottom-[10%] left-1/2 w-1/2 w-[70%] -translate-x-1/2 rounded border bg-zinc-800 p-3"
+				>
+					<label class="flex flex-col justify-center space-y-3">
+						<span class="text-center text-sm"
+							>Adjust diameter of circle at {selected.cx}, {selected.cy}</span
+						>
+						<input type="range" id="radius" bind:value={selected.r} />
+					</label>
+				</div>
+			{/if}
 
 			<!-- svelte-ignore a11y_click_events_have_key_events -->
 			<!-- svelte-ignore a11y_no_static_element_interactions -->
@@ -44,10 +80,18 @@
 					<circle
 						stroke="#fff"
 						stroke-width="2"
-						fill={selected.id === circle.id ? 'gray' : 'transparent'}
+						class={`${selected.id === circle.id ? 'fill-gray-500' : 'fill-transparent'} transition`}
 						{...circle}
 						onclick={(e) => {
-              e.stopPropagation()
+							e.stopPropagation();
+							selected = circle;
+						}}
+						oncontextmenu={(e) => {
+							if (status === 'editing') {
+								snapshot();
+							}
+							e.preventDefault();
+							status = 'editing';
 							selected = circle;
 						}}
 					></circle>
@@ -59,6 +103,6 @@
 
 <style lang="postcss">
 	button {
-		@apply block rounded border border-zinc-900 bg-gradient-to-b from-white via-zinc-200 to-zinc-400 px-7 py-0.5 text-center font-semibold text-zinc-900 transition hover:opacity-90 active:scale-105;
+		@apply block rounded border border-zinc-900 bg-gradient-to-b from-white via-zinc-200 to-zinc-400 px-7 py-0.5 text-center font-semibold text-zinc-900 transition hover:opacity-90 active:scale-105 disabled:opacity-60 disabled:cursor-not-allowed;
 	}
 </style>
